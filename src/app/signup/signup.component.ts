509850ea87a1b58d4ca93cd9cc76ae5bc38dab07
manish <manishk.yadav01@gmail.com>
2018-06-04 10:54:58 +0530

    mocking state and city api

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-signup',
@@ -10,6 +11,8 @@ import { HttpClient } from '@angular/common/http';
 export class SignupComponent implements OnInit {
 
   user = {};
+  stateList = [];
+  cityList = [];
   constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
@@ -28,22 +31,33 @@ export class SignupComponent implements OnInit {
     this.router.navigate(['/home']);
   }
   getStatesList() {
-    this.http.get('http://localhost:8080/vsign-api/location/country/1/states')
-      .subscribe(res => {
-        console.log(res);
-        }, (err) => {
-          console.log(err);
-        }
-      );
+    // this.http.get('http://localhost:8080/vsign-api/location/country/1/states')
+    //   .subscribe(res => {
+    //     console.log(res);
+    //     this.stateList = res;
+    //     }, (err) => {
+    //       console.log(err);
+    //     }
+    //   );
+    this.stateList = [
+      {id : 1, name : "madhya pradesh"},
+      {id : 2, name : "maharastra"},
+      {id : 3, name : "utter pradesh"}
+    ];
   }
-  getCityList(StateId) {
-    this.http.get('http://localhost:8080/vsign-api/location/states/'+StateId+'/cities')
-      .subscribe(res => {
-        console.log(res);
-        }, (err) => {
-          console.log(err);
-        }
-      );
+  public getCityList(StateId) {
+    // this.http.get('http://localhost:8080/vsign-api/location/states/'+StateId+'/cities')
+    //   .subscribe(res => {
+    //     console.log(res);
+    //     }, (err) => {
+    //       console.log(err);
+    //     }
+    //   );
+    console.log(StateId);
+    if(StateId==='1') this.cityList = [{id : 1, name :"bhopal"},{id : 2, name :"Indore"}]
+    else if(StateId===2) this.cityList = [{id : 1, name :"Mumbai"},{id : 2, name :"Pune"}]
+    else if(StateId===3) this.cityList = [{id : 1, name :"Lakhnao"},{id : 2, name :"Kanpur"}]
+    
   }  
 
 }
