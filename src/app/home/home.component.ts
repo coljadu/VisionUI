032079fa2c04b4e1e49f8f423cab15e24796f2e3
manish <manishk.yadav01@gmail.com>
2018-06-04 18:01:33 -0400

    added home page apis

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  name = "";
+  plans = [];
+  constructor(private http: HttpClient, private router: Router) { 
+    
   }
 
+  ngOnInit() {
+    this.getUserInfo();
+    this.getPlanInfo();
+  }
+  getUserInfo() {
+    this.name = 'Ram';
+  }
+  getPlanInfo(){
+    this.http.get('http://localhost:8080/vsign-api/product/plans?status=ACTIVE')
+      .subscribe(res => {
+        console.log(res.data);
+        this.plans = res.data;
+
+      })
+  }
 }
