b00fa9a07b68d21dcb79bf739184d0406a604583
manish <manishk.yadav01@gmail.com>
2018-06-11 16:44:15 -0400

    update

--- src/app/purchase/purchase.component.ts
+++ src/app/purchase/purchase.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-purchase',
@@ -12,6 +13,8 @@ export class PurchaseComponent implements OnInit {
   purchaseData = {};
   Plans:any = [];
   selectedPlan = {};
+  machineId = "";
+
   constructor(private http: HttpClient, private router: Router) {
     
    }
@@ -21,10 +24,21 @@ export class PurchaseComponent implements OnInit {
   }
 
   getPlans() {
-    this.http.get('https://gentle-badger-89.localtunnel.me/vsign-api/product/plans?status=ACTIVE')
+    this.http.get('https://evil-parrot-9.localtunnel.me/vsign-api/product/plans?status=ACTIVE')
       .subscribe(res => {
         console.log(res);
         this.Plans = res;
       })
-  }  
+  } 
+  intitPurchase(){
+    var puchasedData = {};
+    puchasedData.machineId = this.machineId;
+    puchasedData.planId = this.selectedPlan.planId;
+    console.log('purachses data',puchasedData);
+
+    this.http.post('https://evil-parrot-9.localtunnel.me/vsign-api/customers/initiateOrder',puchasedData)
+      .subscribe(res => {
+        console.log(res);
+      })
+  } 
 }
